ce566a7edee33f09cd3fd0a88dc1b06d6d7ca4f6
machine23 <vassili.baranov@gmail.com>
2018-04-05 13:38:15 +0300

    каркас и документация к классу MyTeleBotDB

--- src/MyTeleBot/mytelebot_db.py
+++ src/MyTeleBot/mytelebot_db.py
@@ -8,15 +8,40 @@ class MyTeleBotDB:
     класс для работы с БД. Пока в качестве заглушки работаен напрямую с rss,
     а не с БД.
     '''
-    def get_news(self, key=None):
+    def get_news(self, key='') -> []:
+        '''
+        Возвращает список из словарей, каждый словарь - отдельная новость.
+        Словарь имеет следующие поля:
+        {
+            'title':     -заголовок новости,
+            'published': -таймстамп времени публикации,
+            'link':      -адрес новости,
+            'summary':   -краткое изложение новости,
+            'base':      -адрес rss-канала
+        }
+        Принимает на вход необязательный параметр key (пока не используется)
+        '''
         feed = feedparser.parse(LENTA_FEED)
         news = feed['entries'][:10]
         for n in news:
             n['published'] = self.format_time(n.get('published'))
         return news
 
-    def format_time(self, time_str):
-        return time_str[17:25]
+    def set_news(self, news: dict):
+        '''
+        Принимает обязательный параметр news типа dict.
+        Обязательные поля в словаре:
+        {
+            'title':     -заголовок новости,
+            'published': -таймстамп времени публикации,
+            'link':      -адрес новости,
+            'summary':   -краткое изложение новости,
+            'base':      -адрес rss-канала
+        }
+        '''
+        pass
+
+    
 
 
 if __name__ == '__main__':
